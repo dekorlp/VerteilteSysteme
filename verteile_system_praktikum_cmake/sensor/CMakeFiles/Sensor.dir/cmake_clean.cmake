1ad507cfe313e27939c3265f2e813b0f79f3991b
Dennis <dennis.koehler92@gmx.de>
2017-06-03 20:45:21 +0000

    Einbau der Cmake Konfiguration zum schnellen kompilieren und linken

---
+++ verteile_system_praktikum_cmake/sensor/CMakeFiles/Sensor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sensor.dir/source/Sensor_Client.cpp.o"
+  "CMakeFiles/Sensor.dir/source/Sensor.cpp.o"
+  "CMakeFiles/Sensor.dir/source/MultiCastReceiver.cpp.o"
+  "Sensor.pdb"
+  "Sensor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sensor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
