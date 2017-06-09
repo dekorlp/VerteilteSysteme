ec4a61f43d6ae09e312e8c0f7f95fe7e52e55482
Michael Klimek <michael.1990.k@gmail.com>
2017-06-09 15:56:48 +0200

    Server Sensoren korrigiert. Läuft jetzt alles

--- verteile_system_praktikum_cmake/sensor/source/MultiCastReceiver.cpp
+++ verteile_system_praktikum_cmake/sensor/source/MultiCastReceiver.cpp
@@ -32,7 +32,7 @@ MultiCastReceiver::MultiCastReceiver(boost::asio::io_service& io_service,
     socket_.open(listen_endpoint.protocol());
     socket_.set_option(boost::asio::ip::udp::socket::reuse_address(true));
     socket_.bind(listen_endpoint);
-    //cout << "GEH ST DU REIN? :)" << endl;
+ 
     // Join the multicast group.
     socket_.set_option(
             boost::asio::ip::multicast::join_group(multicast_address));
@@ -40,18 +40,13 @@ MultiCastReceiver::MultiCastReceiver(boost::asio::io_service& io_service,
     // Für Lokal Rechner
     socket_.set_option(boost::asio::ip::multicast::enable_loopback(true));
 
-    //cout << "Port adr:" << multicast_port << endl;
-    //cout << "lisen cast adr:" << listen_address.to_string() << endl;
-    //cout << "Multi cast adr:" << multicast_address.to_string() << endl;
-
-    //cout << "BEFORE RECEIVE " << endl;
+ 
     socket_.async_receive_from(
             boost::asio::buffer(data_, max_length), sender_endpoint_,
             boost::bind(&MultiCastReceiver::handle_receive_from, this,
             boost::asio::placeholders::error,
             boost::asio::placeholders::bytes_transferred));
 
-    //cout << "AFTER RECEIVE " << endl;
 }
 
 void MultiCastReceiver::decodeMessage(string message)
@@ -61,8 +56,6 @@ void MultiCastReceiver::decodeMessage(string message)
         // 3#85;;;;
         // 11#100
         // 0123456789
-        //cout << "Nachricht ist gültig (# gefunden)" << endl;
-        //cout << "decodeMessage: "<< message << endl;
         int sensorNr;
         int sensorValue;
         
@@ -71,11 +64,11 @@ void MultiCastReceiver::decodeMessage(string message)
         message.erase(0, message.find("#", 0)+1);
         sensorValue = stoi(message.substr(0, message.find(";", 0)));
         
-        //cout << "SENSOR NR "<< sensorNr<< endl;
-        //cout << "SENSOR VALUEEE "<< sensorValue<< endl;
         
         m->lock();
         sensorList->at(sensorNr) = sensorValue;
+        cout << "\nSensor Nr. " << sensorNr << "  Value AUFGEFÜLLT: " << sensorValue <<endl;
+
         m->unlock();
         
         
@@ -84,16 +77,13 @@ void MultiCastReceiver::decodeMessage(string message)
 
 void MultiCastReceiver::handle_receive_from(const boost::system::error_code& error, size_t bytes_recvd) {
 
-    //cout << "IN HANDLE RECEIVE FROM " << endl;
-
 
     if (!error) {
-        //cout << "IN HANDLE RECEIVE FROM != ERROR" << endl;
         
         
         decodeMessage(data_);
         
-        //sensorList->at(2) = 40;
+       
        
         socket_.async_receive_from(
                 boost::asio::buffer(data_, max_length), sender_endpoint_,
